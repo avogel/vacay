ae92892a39f8b03dc88bda1f6759b2aae902b79d
Ari <avogel@mit.edu>
2013-01-17 19:36:19 -0500

    added simple templates as placeholders

--- vacay/settings.py
+++ vacay/settings.py
@@ -1,4 +1,5 @@
 # Django settings for vacay project.
+import os.path
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -103,6 +104,7 @@ ROOT_URLCONF = 'vacay.urls'
 WSGI_APPLICATION = 'vacay.wsgi.application'
 
 TEMPLATE_DIRS = (
+    os.path.join(os.path.dirname(__file__), 'templates').replace('\\','/'),
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
