36ff593d3537c60f216f4a4576ec929cec4f0abb
Preston <pwrtsoccer@gmail.com>
2013-01-21 21:57:12 -0500

    changing import to vacay

--- vacay/urls.py
+++ vacay/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import *
-from django.conf import settings
+from vacay import settings
+
+print "settings.BASE_URL is: " + settings.BASE_URL 
 
 urlpatterns = patterns('',
 	(r'^%s' % settings.BASE_URL, include('vacay.vacay_urls')),
