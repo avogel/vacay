66c79d1d789d2a50f6f714a651443e2343855d60
Preston <pwrtsoccer@gmail.com>
2013-01-22 20:47:22 -0500

    changed all links to start with /vacay\nstarted the blogging template

--- vacay/views/blogging_view.py
+++ vacay/views/blogging_view.py
@@ -1,9 +1,6 @@
-from django.http import HttpResponse
-import datetime
+from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def blogging(request):
-    now = datetime.datetime.now()
-    html = "<html><body>It is now %s.</body></html>" % now
-    return HttpResponse(html)
+    return render_to_response('blogging.html', {})
