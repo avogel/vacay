dc467e5b76072d347e33339b24f37f1d9fe7ad1f
Ari <avogel@mit.edu>
2013-01-25 14:51:11 -0500

    bookmarks front-end works

--- vacay/views/viewpost_view.py
+++ vacay/views/viewpost_view.py
@@ -4,6 +4,8 @@ from django.shortcuts import render
 from vacay.vposts.models import *
 import datetime
 
+
+
 def viewpost(request, id):
 	try:
 		id = int(id)
@@ -18,8 +20,11 @@ def viewpost(request, id):
 		days = {}
 		output = {}
 		for trip in trips:
-			cities[trip] = VisitedCity.objects.filter(trip = trip)
+			#cities[trip] = VisitedCity.objects.filter(trip = trip)
+			output[trip] = VisitedCity.objects.filter(trip = trip)
 			for city in cities:
 				ds = VisitedDay.objects.filter(visited_city=city)
-				days[city]=ds
-	return render(request, 'viewpost.html', {'post' : post,'trips':trips,'cities':cities, 'days':days})
+				#days[city]=ds
+				output[trip][city]=VisitedDay.objects.filter(visited_city=city)
+
+	return render(request, 'viewpost.html', {'post' : post,'trips':trips,'cities':cities, 'output':output})
