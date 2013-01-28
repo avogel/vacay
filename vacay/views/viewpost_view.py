59c5e187dc380441d9f0510e25ca94182f253229
Preston <pwrtsoccer@gmail.com>
2013-01-27 23:29:44 -0500

    got the dropdown to work, fuck ajax

--- vacay/views/viewpost_view.py
+++ vacay/views/viewpost_view.py
@@ -1,19 +1,35 @@
 from django.template.loader import get_template
-from django.http import Http404
-from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.shortcuts import *
 from vacay.vposts.models import *
-import datetime
+from django.utils import simplejson
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
-
+@ensure_csrf_cookie
 def viewpost(request, id):
 	try:
 		id = int(id)
-		print id
 	except ValueError:
 		raise Http404()
 	post = Post.objects.get(id=id)
 	user = request.user
+	if request.method == 'POST':
+		type = request.POST["type"]
+		bookmark_id = request.POST["id"]
+		if type == 'trip':
+			trip = Trip.objects.get(id=bookmark_id)
+			if not trip.ideas.contains(post):
+				trip.ideas.add(post)
+		elif type == 'city':
+			city = VisitedCity.objects.get(id=bookmark_id)
+			if not city.ideas.contains(post):
+				city.ideas.add(post)
+		else:
+			day = VisitedDay.objects.get(id=bookmark_id)
+			if not day.ideas.contains(post):
+				day.ideas.add(post)
+		return HttpResponse(simplejson.dumps())
 	if user.is_authenticated():
 		trips = Trip.objects.filter(user=user)
 		cities = {}
